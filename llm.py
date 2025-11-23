d52a27b66f7c823d5a057d403781c5c2dc7b6b91
mraitan1 <mraitan1@students.towson.edu>
2025-11-23 18:35:43 -0500

    Fixed OpenAI project ket changes

--- llm.py
+++ llm.py
@@ -1,19 +1,19 @@
 from openai import OpenAI
 from dotenv import load_dotenv
+import os
 import streamlit as st
 
 load_dotenv()
 
-api_key = st.secrets.get("OPENAI_API_KEY", None)
+api_key = st.secrets.get("OPENAI_API_KEY")
+project_id = st.secrets.get("OPENAI_PROJECT")
 
-if api_key is None:
-    # Local development: fallback to .env
-    from dotenv import load_dotenv
+if api_key or project_id is None:
     load_dotenv()
-    import os
     api_key = os.getenv("OPENAI_API_KEY")
+    project_id = os.getenv("OPENAI_PROJECT")
 
-client = OpenAI(api_key=api_key)
+client = OpenAI(api_key=api_key,project=project_id)
 
 def ask_llm(prompt, model="gpt-4.1-mini"):
 
